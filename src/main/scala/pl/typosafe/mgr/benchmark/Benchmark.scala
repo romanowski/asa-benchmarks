bc859e1e01ae421b788184ffb04be575c77306df
Krzysztof Romanowski <romanowski.kr@gmail.com>
2015-07-06 21:20:48 +0200

    working launch and able to run from scalaIDE

--- src/main/scala/pl/typosafe/mgr/benchmark/Benchmark.scala
+++ src/main/scala/pl/typosafe/mgr/benchmark/Benchmark.scala
@@ -7,27 +7,24 @@ import scala.concurrent.{Await, Future}
 import scala.concurrent.duration._
 
 class Benchmark(name: String, timeout: Long = 100L) {
-  protected val system = ActorSystem(name)
+  protected val system = ActorSystem(name.replace(' ', '-'))
 
   protected val defaultTestTime = 10000L
 
   protected implicit val timeoutDuration = Timeout(timeout millis)
 
 
-  def reportResult(result: String): Unit = {
-    println(s"Benchmark: $name ends with:\n$result")
-  }
-
-  def reportFailure(message: String): Unit = {
-    println(s"Benchmark: $name failed due to:\n $message")
+  def reportResult(result: String, success: Boolean): Unit = {
+    val successMgs = if(success) "ends" else "failed"
+    println(s"Benchmark: $name $successMgs with:\n$result")
   }
 
   def doTest(doBenchmark: => Future[String]): Unit = {
     try {
-      reportResult(Await.result(doBenchmark, timeout millis))
+      reportResult(Await.result(doBenchmark, timeout millis), true)
     } catch {
       case exception: Throwable =>
-        reportFailure(exception.getMessage)
+        reportResult(exception.getMessage, false)
     }
     system.shutdown()
   }
